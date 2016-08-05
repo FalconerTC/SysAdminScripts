22b90f3867cff57f9571f9a8e6d888e5a8435dde
Zachariah Bendas <zbendas@yahoo.com>
2016-08-05 12:44:53 -0500

    Configuration file and file factory made. Implementation into control.py begun.

---
+++ configbuilder.py
@@ -0,0 +1,26 @@
+import configparser
+from configparser import ExtendedInterpolation
+config = configparser.ConfigParser(interpolation=ExtendedInterpolation())
+config['DEFAULT'] = {}
+# ${section:key} syntax below designates interpolation of key from section
+config['Common'] = {'home_dir': '/home/spig',
+                    'servers_dir': '${home_dir}/servers',
+                    'scripts_dir': '${home_dir}/scripts',
+                    'settings_dir': '${scripts_dir}/settings',
+                    'log_dir': '${home_dir}/log'}
+config['Starbound'] = {'game_dir': '${Common:servers_dir}/starbound',
+                       'game_path': '${game_dir}/linux',
+                       'game_id': 211820,
+                       'screen_name': 'Starbound_Server',
+                       'start_cmd': '${game_path}/starbound_server',
+                       'steam_username': 'spider_pig448'}
+config['KillingFloor'] = {'game_dir': '${Common:servers_dir}/killingfloor',
+                          'game_path': '${game_dir}/System',
+                          'game_id': 215360,
+                          'screen_name': 'KillingFloor_Server',
+                          'start_flags': 'KF-farm.rom?VACSecured=true?AdminName=spig?AdminPassword=nyanpasu',
+                          'start_cmd': '${game_path}/ucc-bin server ${start_flags} -ini=Killingfloor.ini -nohomedir',
+                          'steam_username': 'spig_server'}
+
+with open('config.cfg', 'w') as configfile:
+    config.write(configfile)
